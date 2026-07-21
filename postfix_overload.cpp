ff36325fa5df6c09b103255e5ea20576a4836438
Sandesh <kattelsandesh99@gmail.com>
2026-07-21 06:24:44 +0545

    Create postfix_overload.cpp

---
+++ postfix_overload.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class test
+{
+    int a;
+    public:
+    test()
+    {
+        a=0;
+    }
+    void operator ++(int)
+    {
+        a++;
+    }
+    void operator --(int)
+    {
+        a--;
+    }
+    void show()
+    {
+        cout<<"a="<<a<<endl;
+    }
+};
+int main()
+{
+    test t;
+    t++;
+    t.show();
+    t--;
+    t.show();
+}
